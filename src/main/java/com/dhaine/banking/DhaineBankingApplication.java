47b27f4e8f46f2a0852bf9b2ec76dba19202905d
daudu <jdaudu@digicoreltd.com>
2026-03-19 23:22:47 +0100

    feat(setup): initialize service

---
+++ src/main/java/com/dhaine/banking/DhaineBankingApplication.java
@@ -0,0 +1,12 @@
+package com.dhaine.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhaineBankingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DhaineBankingApplication.class, args);
+  }
+}
---
+++ src/test/java/com/dhaine/banking/DhaineBankingApplicationTests.java
@@ -0,0 +1,11 @@
+package com.dhaine.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhaineBankingApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
