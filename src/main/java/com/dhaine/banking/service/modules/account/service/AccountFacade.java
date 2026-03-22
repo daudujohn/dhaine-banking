f416d737111d15a96db07b7a979264a75d9edffb
daudu <jdaudu@digicoreltd.com>
2026-03-22 04:02:35 +0100

    feat(account): implement account info, updates

---
+++ src/main/java/com/dhaine/banking/service/modules/account/service/AccountFacade.java
@@ -0,0 +1,34 @@
+package com.dhaine.banking.service.modules.account.service;
+
+import com.dhaine.banking.service.modules.account.dto.AccountDTO;
+import com.dhaine.banking.service.modules.account.dto.CreateAccountRequest;
+import com.dhaine.banking.service.modules.transaction.dto.TransactionRecordDTO;
+import com.dhaine.banking.service.modules.transaction.service.TransactionHistoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Daudu John
+ * @createdOn Mar-20(Fri)-2026
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AccountFacade {
+  private final AccountService accountService;
+  private final TransactionHistoryService transactionHistoryService;
+
+  public AccountDTO createAccount(CreateAccountRequest createAccountRequest) {
+    AccountDTO accountDTO = accountService.createAccount(createAccountRequest);
+
+    TransactionRecordDTO transactionRecordDTO = new TransactionRecordDTO();
+    transactionRecordDTO.setTransactionType("CREDIT");
+    transactionRecordDTO.setAmount(accountDTO.getBalance());
+    transactionRecordDTO.setAccountBalance(accountDTO.getBalance());
+    transactionHistoryService.createTransactionHistory(
+        accountDTO.getAccountNumber(), transactionRecordDTO);
+
+    return accountDTO;
+  }
+}
--- src/main/java/com/dhaine/banking/service/modules/account/service/AccountService.java
+++ src/main/java/com/dhaine/banking/service/modules/account/service/AccountService.java
@@ -1,32 +1,79 @@
 package com.dhaine.banking.service.modules.account.service;
 
+import com.dhaine.banking.core.api.exception.DhaineException;
 import com.dhaine.banking.service.modules.account.dto.AccountDTO;
 import com.dhaine.banking.service.modules.account.dto.CreateAccountRequest;
-import java.time.LocalDateTime;
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
  * @author Daudu John
  * @createdOn Mar-20(Fri)-2026
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AccountService {
+  private final PasswordEncoder passwordEncoder;
   private final Map<String, AccountDTO> accounts = new HashMap<>();
+  private final SecureRandom random = new SecureRandom();
 
   public AccountDTO createAccount(CreateAccountRequest createAccountRequest) {
     AccountDTO accountDTO = new AccountDTO();
     accountDTO.setAccountName(createAccountRequest.getAccountName());
-    accountDTO.setAccountPassword(createAccountRequest.getAccountPassword());
+    accountDTO.setAccountPassword(
+        passwordEncoder.encode(createAccountRequest.getAccountPassword()));
     accountDTO.setBalance(createAccountRequest.getInitialDeposit());
-    //            TODO: make it globally random
-    accountDTO.setAccountNumber(LocalDateTime.now().toString());
+    accountDTO.setAccountNumber(generateAccountNumber());
 
     accounts.put(accountDTO.getAccountNumber(), accountDTO);
 
+    log.info(ObjectUtils.toString(accounts));
+
     return accountDTO;
+  }
+
+  public AccountDTO retrieveAccount(String accountNumber) {
+    AccountDTO accountDTO = accounts.get(accountNumber);
+
+    if (accountDTO == null) throw new DhaineException("Account not found");
+
+    return accountDTO;
+  }
+
+  public AccountDTO retrieveAccountInformation(String accountNumber) {
+    AccountDTO accountDTO = this.retrieveAccount(accountNumber);
+    accountDTO.setAccountPassword(null);
+    return accountDTO;
+  }
+
+  public AccountDTO updateAccount(AccountDTO accountDTO) {
+    AccountDTO accountToUpdate = this.retrieveAccount(accountDTO.getAccountNumber());
+
+    accountToUpdate.setBalance(accountDTO.getBalance());
+
+    accounts.put(accountDTO.getAccountNumber(), accountToUpdate);
+
+    return accountToUpdate;
+  }
+
+  private static String generateAccountNumber() {
+    String numbers = "0123456789";
+    StringBuilder sb = new StringBuilder();
+    Random random = new SecureRandom();
+
+    for (int i = 0; i < 10; i++) {
+      int index = random.nextInt(numbers.length());
+      sb.append(numbers.charAt(index));
+    }
+
+    return sb.toString();
   }
 }
